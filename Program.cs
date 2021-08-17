9fde33ed505e7e1b24fd0b5362234805dbfca7f1
sukhdev2000[ <dsukhdev2000@gmail.com>
2021-08-17 21:17:09 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace interfaceconcept
+{
+    interface Ifirst
+    {
+        void fun1();
+    }
+    class first:Ifirst
+    {
+     public void fun1()
+      {
+          System.Console.WriteLine("Hey I'm from interface1!!");
+      }
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var obj=new first();
+          Ifirst obj1=new first();
+          obj.fun1();
+          obj1.fun1();
+        }
+    }
+}
